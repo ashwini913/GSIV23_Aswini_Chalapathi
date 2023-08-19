265326339b41febeac756327b211e8d1cb9f9003
aswini <gvc.ashwini@gmail.com>
2023-08-19 15:43:26 +0530

    Updates

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { movieListReducer } from "./movieListReducer";
-
-const reducer = combineReducers({
-  movieList: movieListReducer,
-});
-
-export default reducer;
